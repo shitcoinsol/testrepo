5db7c9ae711314d741cb134a89f37fcbb7e7f009
shitcoinsol <164472047+shitcoinsol@users.noreply.github.com>
2025-06-27 20:45:13 +0900

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,7 +1,6 @@
 
 export default async function handler(req, res) {
   const imageUrl = req.body.imageUrl;
-
   const replicateKey = process.env.REPLICATE_API_TOKEN;
   const openaiKey = process.env.OPENAI_API_KEY;
 
